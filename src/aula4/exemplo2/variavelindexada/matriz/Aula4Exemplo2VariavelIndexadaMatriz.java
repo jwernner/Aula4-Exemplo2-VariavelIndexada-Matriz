4e055c393d800a215bb59f2b468a2aa6f0978b45
jwernner <jorge.werner@gmail.com>
2026-04-14 16:11:17 -0300

    motagem do projeto

--- src/aula4/exemplo2/variavelindexada/matriz/Aula4Exemplo2VariavelIndexadaMatriz.java
+++ src/aula4/exemplo2/variavelindexada/matriz/Aula4Exemplo2VariavelIndexadaMatriz.java
@@ -4,6 +4,8 @@
  */
 package aula4.exemplo2.variavelindexada.matriz;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jwern
@@ -15,6 +17,19 @@ public class Aula4Exemplo2VariavelIndexadaMatriz {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
-}
+                  double A[ ][ ] = new double[3][4];
+          int i, j;
+
+      // leitura dos valores
+      for(i=0; i<3; i++) {
+             for (j=0; j<4; j++){
+               A[i][j] = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da linha "+i+" e coluna "+j+": "));
+             }
+      }
+          
+      // escrita dos valores
+      for(i=0; i<3; i++){
+         for (j=0; j<4; j++){
+            JOptionPane.showMessageDialog(null, "O valor contido na linha " +i+" e coluna "+j+ " eh " + A[i][j]);
+         } }
+  } }
