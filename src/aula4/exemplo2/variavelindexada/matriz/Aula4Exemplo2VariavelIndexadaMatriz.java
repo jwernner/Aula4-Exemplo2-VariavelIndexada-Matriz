e1c00905d8c07e11710de897707253ed9acfa221
jwernner <jorge.werner@gmail.com>
2026-04-14 15:55:55 -0300

    motagem do projeto

---
+++ src/aula4/exemplo2/variavelindexada/matriz/Aula4Exemplo2VariavelIndexadaMatriz.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula4.exemplo2.variavelindexada.matriz;
+
+/**
+ *
+ * @author jwern
+ */
+public class Aula4Exemplo2VariavelIndexadaMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
